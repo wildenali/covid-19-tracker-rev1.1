8bb3596aa4f8049d518c6720669e9debaa50b2be
wildenali <wildenwildenaliali@gmail.com>
2020-12-07 10:00:19 +0700

    Membuat Judul dan Dropdown simple untuk nantinya buat milih negara

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React from "react";
+import { MenuItem, FormControl, Select, Menu } from '@material-ui/core'
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Covid-19 Tracker V1.1</h1>
 
       {/* Header */}
-      {/* Title + Selected Input dropdown field */}
+      <div className="app_header">
+        <h1>Corona Virus Tracker</h1>
+
+        {/* Title + Selected Input dropdown field */}
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="z">Worldwide</MenuItem>
+            <MenuItem value="a">Option 2</MenuItem>
+            <MenuItem value="b">Option 3</MenuItem>
+            <MenuItem value="c">Option 4</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
 
       {/* InfoBox Cases */}
       {/* InfoBox Recovered*/}
