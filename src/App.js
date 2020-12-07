44afe6ad779f0ab3798bcfaef0bb26fcb31f0a8b
wildenali <wildenwildenaliali@gmail.com>
2020-12-07 09:49:43 +0700

    Structure of web covid-19 tracker

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Cek</h1>
+      <h1>Covid-19 Tracker V1.1</h1>
+
+      {/* Header */}
+      {/* Title + Selected Input dropdown field */}
+
+      {/* InfoBox Cases */}
+      {/* InfoBox Recovered*/}
+      {/* InfoBox Deaths*/}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
