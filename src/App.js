2792ea5567fb018d27a1f8fbed330ee9bf033015
wildenali <wildenwildenaliali@gmail.com>
2020-12-10 09:50:43 +0700

    Dropdown, tambah dropdown untuk pilih Area

--- src/App.js
+++ src/App.js
@@ -21,6 +21,14 @@ function App() {
             <MenuItem value="c">Option 4</MenuItem>
           </Select>
         </FormControl>
+        <FormControl className="app_dropdown_border_virtual">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="area_1">Area 1</MenuItem>
+            <MenuItem value="area_2">Area 2</MenuItem>
+            <MenuItem value="area_3">Area 3</MenuItem>
+            <MenuItem value="area_4">Area 4</MenuItem>
+          </Select>
+        </FormControl>
       </div>
 
 
